90e160743e1e6b12157955f61071a7cadde4a263
Maksym <maksymnikitiuk2014@gmail.com>
2025-01-02 22:41:11 +0200

    Add CreateSignETAMPService project for modularity
    Introduced the CreateSignETAMPService project to enhance solution structure and maintainability. Updated dependencies, project configurations, and integrated related services. Minor changes were made to existing components for consistency.

---
+++ Examples/ETAMP.Console.CreateSignETAMPService/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography;
+using ETAMP.Console.CreateETAMPService.Models;
+using ETAMP.Core.Models;
+using ETAMP.Encryption.Interfaces.ECDSAManager;
+using ETAMP.Extension.Builder;
+using ETAMP.Wrapper.Base;
+using Microsoft.Extensions.DependencyInjection;
+
+class Program
+{
+    private static ServiceProvider _provider;
+
+    static void Main(string[] args)
+    {
+        _provider = Service.ConfigureServices();
+        Console.WriteLine(SignETAMP(_provider).ToJson());
+    }
+
+    public static ETAMPModel<TokenModel> SignETAMP(ServiceProvider provider)
+    {
+        var sign = provider.GetService<SignWrapperBase>();
+        var creator = provider.GetService<IECDsaCreator>();
+        sign.Initialize(creator.CreateECDsa(), HashAlgorithmName.SHA512);
+        var etamp = Service.CreateETAMP(_provider);
+        etamp.Sign(sign);
+        return etamp;
+    }
+}
